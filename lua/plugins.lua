586d7d56c3027e0bd8e09c1c37c9d77363946d9b
Diego Weinmann <diegoweinmann@gmail.com>
2021-08-03 00:49:00 -0300

    update plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,17 +8,26 @@ if fn.empty(fn.glob(install_path)) > 0 then
   execute 'packadd packer.nvim'
 end
 
-return require('packer').startup(function()
+
+
+return require('packer').startup(function(use)
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+  use {'wbthomason/packer.nvim', opt=true }
+
   use {'dracula/vim', as = 'dracula'}
+
   use {
     'kyazdani42/nvim-tree.lua',
     requires = 'kyazdani42/nvim-web-devicons'
   }
+
   use 'neovim/nvim-lspconfig'
+
   use 'hrsh7th/nvim-compe'
+
   use 'hrsh7th/vim-vsnip'
+
 end)
+
 
 
