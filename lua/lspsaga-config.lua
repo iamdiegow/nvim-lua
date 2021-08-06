e11d6cadce9a27f5a5a2390b6fd20192d0b7b55c
Diego Weinmann <diegoweinmann@gmail.com>
2021-08-06 02:07:58 -0300

    update lspsaga config

--- lua/lspsaga-config.lua
+++ lua/lspsaga-config.lua
@@ -9,10 +9,6 @@ saga.init_lsp_saga {
 
 }
 
-vim.api.nvim_set_keymap('n', '<Leader>D', ':Lspsaga diagnostics_jump_next<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'K', ':Lspsaga hover_doc<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<C-k>', ':Lspsaga signature_help<CR>', { noremap = true, silent = true })
-
 
 vim.cmd("")
 vim.cmd([[
@@ -21,8 +17,8 @@ vim.cmd([[
 	nnoremap <silent> ]e <cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_next()<CR>
 	nnoremap <silent> <C-f> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>
 	nnoremap <silent> <C-b> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>
-	nnoremap <silent> gs :Lspsaga signature_help<CR>
-	nnoremap <silent> GD :Lspsaga preview_definition<CR>
+	nnoremap <silent> gs :Lspsaga signature_help()<CR>
+	nnoremap <silent> GD :Lspsaga preview_definition()<CR>
 	nnoremap <silent><leader>cd <cmd>lua require'lspsaga.diagnostic'.show_line_diagnostics()<CR>
 	nnoremap <silent> <C-f> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>
 	nnoremap <silent> <C-b> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>
