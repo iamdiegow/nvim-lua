1b7c7fdc8eb1e3faeafbc18e37bdf76b92aee2a8
Diego Weinmann <diegoweinmann@gmail.com>
2021-08-05 22:50:06 -0300

    change background

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -3,14 +3,14 @@ vim.cmd([[colo material]])
 -- Material Config
 -- variants = 'darker' | 'lighter' | 'oceanic' | 'palenight' | 'deep ocean'
 -- lualine -> theme = 'material-nvim'
-vim.g.material_style = 'oceanic'
+vim.g.material_style = 'palenight'
 vim.g.material_italic_comments = true
 vim.g.material_italic_keywords = true
 vim.g.material_italic_functions = true
 vim.g.material_italic_variables = false
 vim.g.material_contrast = true
 vim.g.material_borders = true
-vim.g.material_disable_background = true
+vim.g.material_disable_background = false
 
 require('material').set()
 
