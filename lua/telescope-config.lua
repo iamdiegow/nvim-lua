7f70a382596651a88b8738cc8ee0f89f0e5eed18
Diego Weinmann <diegoweinmann@gmail.com>
2021-08-07 13:19:25 -0300

    add telescope keymappings

--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -3,6 +3,13 @@ vim.cmd([[
 	nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
 	nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
 	nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
+	nnoremap <leader>fs <cmd>lua require('telescope.builtin').lsp_document_symbols()<cr>
+	nnoremap <leader>fk <cmd>lua require('telescope.builtin').keymaps()<cr>
+
+	nnoremap <leader>gs <cmd>lua require('telescope.builtin').git_status()<cr>
+	nnoremap <leader>gc <cmd>lua require('telescope.builtin').git_commits()<cr>
+	nnoremap <leader>gf <cmd>lua require('telescope.builtin').git_files()<cr>
+	nnoremap <leader>gb <cmd>lua require('telescope.builtin').git_branches()<cr>
 ]])
 
 local actions = require('telescope.actions')
