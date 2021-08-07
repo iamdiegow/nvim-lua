96bbe443142432f267fdaa1ff1ab4cd75824bac2
Diego Weinmann <diegoweinmann@gmail.com>
2021-08-07 01:31:33 -0300

    add indent line options

--- lua/indent-blankline-config.lua
+++ lua/indent-blankline-config.lua
@@ -1,4 +1,11 @@
 require("indent_blankline").setup {
-    char = "|",
-    buftype_exclude = {"terminal"}
+    char = "¦",
+    		buftype_exclude = {"terminal"}
 }
+
+vim.cmd([[
+	let g:indent_blankline_char = ''
+	let g:indent_blankline_use_treesitter = v:true
+	let g:indent_blankline_show_first_indent_level = v:false
+]])
+
