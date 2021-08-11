2bb8dbe6a37b77553a78e72e789ea3c48f81087f
Diego Weinmann <diegoweinmann@gmail.com>
2021-08-10 23:18:17 -0300

    add vscode theme settings to bufferline

--- lua/bufferline-config.lua
+++ lua/bufferline-config.lua
@@ -30,7 +30,53 @@ end,
     separator_style = "thick",
     enforce_regular_tabs = false,
     always_show_bufferline = true,
-    sort_by = 'id'
+    sort_by = 'id',
+		-- for vscode theme
+		highlights = {
+			fill = {
+				guifg = {attribute = "fg", highlight = "Normal"},
+				guibg = {attribute = "bg", highlight = "StatusLineNC"},
+			},
+			background = {
+				guifg = {attribute = "fg", highlight = "Normal"},
+				guibg = {attribute = "bg", highlight = "StatusLine"}
+			},
+			buffer_visible = {
+				gui = "",
+							guifg = {attribute = "fg", highlight="Normal"},
+							guibg = {attribute = "bg", highlight = "Normal"}
+			},
+			buffer_selected = {
+				gui = "",
+							guifg = {attribute = "fg", highlight="Normal"},
+							guibg = {attribute = "bg", highlight = "Normal"}
+			},
+			separator = {
+				guifg = {attribute = "bg", highlight = "Normal"},
+				guibg = {attribute = "bg", highlight = "StatusLine"},
+			},
+			separator_selected = {
+							guifg = {attribute = "fg", highlight="Special"},
+							guibg = {attribute = "bg", highlight = "Normal"}
+			},
+			separator_visible = {
+				guifg = {attribute = "fg", highlight = "Normal"},
+				guibg = {attribute = "bg", highlight = "StatusLineNC"},
+			},
+			close_button = {
+				guifg = {attribute = "fg", highlight = "Normal"},
+				guibg = {attribute = "bg", highlight = "StatusLine"}
+			},
+			close_button_selected = {
+							guifg = {attribute = "fg", highlight="normal"},
+							guibg = {attribute = "bg", highlight = "normal"}
+			},
+			close_button_visible = {
+							guifg = {attribute = "fg", highlight="normal"},
+							guibg = {attribute = "bg", highlight = "normal"}
+			},
+		}
+		-- for vscode theme end
   }
 }
 
