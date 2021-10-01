cd08322ddff20f996f68da110076d36f27d3ada4
Diego Weinmann <diegoweinmann@gmail.com>
2021-10-01 20:31:30 -0300

    fix nvim-tree, after last plugin update

--- lua/nvim-tree-config.lua
+++ lua/nvim-tree-config.lua
@@ -4,58 +4,10 @@ if not present then
 end
 
 local tree_cb = tree_c.nvim_tree_callback
+
 local g = vim.g
 
-g.nvim_tree_side = "left"
-g.nvim_tree_width = 30
-g.nvim_tree_ignore = {".git", ".cache"}
-g.nvim_tree_gitignore = 1
-g.nvim_tree_auto_ignore_ft = {"dashboard"} -- don't open tree on specific fiypes.
-g.nvim_tree_auto_open = 0
-g.nvim_tree_auto_close = 1 -- closes tree when it's the last window
-g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
-g.nvim_tree_follow = 1
-g.nvim_tree_indent_markers = 0
-g.nvim_tree_hide_dotfiles = 1
-g.nvim_tree_git_hl = 1
-g.nvim_tree_highlight_opened_files = 0
-g.nvim_tree_root_folder_modifier = table.concat {":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??"}
-g.nvim_tree_tab_open = 0
-g.nvim_tree_allow_resize = 1
-g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
-g.nvim_tree_disable_netrw = 1
-g.nvim_tree_hijack_netrw = 0
-g.nvim_tree_update_cwd = 1
-g.nvim_tree_lsp_diagnostics = 1
-
-g.nvim_tree_show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows= 1
-}
-g.nvim_tree_icons = {
-    default = "",
-    symlink = "",
-    git = {
-        unstaged = "✗",
-        staged = "✓",
-        unmerged = "",
-        renamed = "➜",
-        untracked = "★",
-        deleted = "",
-        ignored = "◌"
-    },
-    folder = {
-        default = "",
-        open = "",
-        empty = "",
-        empty_open = "",
-        symlink_open = ""
-    }
-}
-
-g.nvim_tree_bindings = {
+local nvim_tree_bindings = {
     {key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit")},
     {key = {"<2-RightMouse>", "<C-]>"}, cb = tree_cb("cd")},
     {key = "<C-v>", cb = tree_cb("vsplit")},
@@ -89,6 +41,80 @@ g.nvim_tree_bindings = {
     {key = "q", cb = tree_cb("close")},
     {key = "g?", cb = tree_cb("toggle_help")}
 }
+
+require('nvim-tree').setup {
+  disable_netrw       = true,
+  hijack_netrw        = true,
+  open_on_setup       = false,
+  ignore_ft_on_setup  = {},
+	auto_open = true,
+  auto_close          = true,
+  open_on_tab         = false,
+  update_to_buf_dir   = true,
+  hijack_cursor       = false,
+  update_cwd          = true,
+  lsp_diagnostics     = true,
+  update_focused_file = {
+    enable      = false,
+    update_cwd  = false,
+    ignore_list = {}
+  },
+  system_open = {
+    cmd  = nil,
+    args = {}
+  },
+
+  view = {
+    width = 10,
+    side = 'bottom',
+    auto_resize = false,
+    mappings = {
+      custom_only = false,
+      list = nvim_tree_bindings
+    }
+  }
+}
+
+
+g.nvim_tree_ignore = {".git", ".cache", "node_modules"}
+g.nvim_tree_gitignore = 1
+g.nvim_tree_auto_ignore_ft = {"dashboard"}
+g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
+g.nvim_tree_indent_markers = 0
+g.nvim_tree_hide_dotfiles = 1
+g.nvim_tree_git_hl = 1
+g.nvim_tree_highlight_opened_files = 0
+g.nvim_tree_root_folder_modifier = table.concat {":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??"}
+g.nvim_tree_allow_resize = 1
+g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
+
+g.nvim_tree_show_icons = {
+    git = 1,
+    folders = 1,
+    files = 1,
+    folder_arrows= 1
+}
+g.nvim_tree_icons = {
+    default = "",
+    symlink = "",
+    git = {
+        unstaged = "✗",
+        staged = "✓",
+        unmerged = "",
+        renamed = "➜",
+        untracked = "★",
+        deleted = "",
+        ignored = "◌"
+    },
+    folder = {
+        default = "",
+        open = "",
+        empty = "",
+        empty_open = "",
+        symlink_open = ""
+    }
+}
+
 
 -- hide statusline when nvim tree is opened
 vim.cmd [[au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == "NvimTree" | set laststatus=0 | else | set laststatus=2 | endif]]
