d94284affa77423189c855417b8fc67127f9ed7c
Diego Weinmann <diegoweinmann@gmail.com>
2021-10-28 22:16:22 -0300

    remove nvim-autopairs cmp config

--- lua/nvim-autopairs-config.lua
+++ lua/nvim-autopairs-config.lua
@@ -2,13 +2,3 @@ require('nvim-autopairs').setup({
   disable_filetype = { "TelescopePrompt" , "vim" },
 	check_ts = true
 })
-require('nvim-autopairs.completion.cmp').setup({
-	map_cr = true,
-	map_complete = true,
-	auto_select = true,
-	insert = false,
-	map_char = {
-		all = '(',
-		tex = '{'
-	}
-})
