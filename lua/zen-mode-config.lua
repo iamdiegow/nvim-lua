4663aec8c61622f041529d6cc22542e74618eb05
Diego Weinmann <diegoweinmann@gmail.com>
2021-11-14 02:00:45 -0300

    zen-mode: update zen-mode config

--- lua/zen-mode-config.lua
+++ lua/zen-mode-config.lua
@@ -1,3 +1,18 @@
-require('twilight').setup({})
+require('zen-mode').setup({
+	window = {
+		backdrop = 0.60,
+		width = 80,
+		height = 1,
+		options = {
+			number = false,
+			cursorline = true,
+			cursorcolumn = true
+		}
+	},
+	plugins = {
+		twilight = { enabled = false },
+		gitsigns = { enabled = false }
+	}
+})
 
-require('zen-mode').setup({})
+require('twilight').setup({})
