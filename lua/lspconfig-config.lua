157544078eafd57219bc11efb3cd8fe3b1b747a1
Diego Weinmann <diegoweinmann@gmail.com>
2021-11-24 00:01:25 -0300

    lspconfig: add json schemas

--- lua/lspconfig-config.lua
+++ lua/lspconfig-config.lua
@@ -118,11 +118,42 @@ require'lspconfig'.dockerls.setup{
 -- JSON npm i -g vscode-langservers-extracted
 require'lspconfig'.jsonls.setup({
 	capabilities = capabilities,
-  commands = {
-		Format = {
-			function()
-				vim.lsp.buf.range_formatting({},{0,0},{vim.fn.line("$"),0})
-			end
+	filetypes = {"json", "jsonc"},
+	settings = {
+		json = {
+			schemaDownload = {
+				enable = true
+			},
+			schemas = {
+				{
+					fileMatch = {"package.json"},
+					url = "https://json.schemastore.org/package.json"
+				},
+				{
+					fileMatch = {"tsconfig*.json"},
+					url = "https://json.schemastore.org/tsconfig.json"
+				},
+				{
+          fileMatch = {
+            ".prettierrc",
+            ".prettierrc.json",
+            "prettier.config.json"
+          },
+          url = "https://json.schemastore.org/prettierrc.json"
+        },
+        {
+          fileMatch = {".eslintrc", ".eslintrc.json"},
+          url = "https://json.schemastore.org/eslintrc.json"
+        },
+        {
+          fileMatch = {".babelrc", ".babelrc.json", "babel.config.json"},
+          url = "https://json.schemastore.org/babelrc.json"
+        },
+        {
+          fileMatch = {"lerna.json"},
+          url = "https://json.schemastore.org/lerna.json"
+        }
+			}
 		}
 	}
 })
