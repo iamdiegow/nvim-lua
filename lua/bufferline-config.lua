1beffbcf7833ea5e9817292bd4e7b8ed7b60c3e5
Diego Weinmann <diegoweinmann@gmail.com>
2021-12-15 00:13:05 -0300

    bufferline: update config

--- lua/bufferline-config.lua
+++ lua/bufferline-config.lua
@@ -1,14 +1,16 @@
-require("bufferline").setup{
+local status_ok, bufferline = pcall(require, 'bufferline')
+if not status_ok then
+	return
+end
+
+bufferline.setup{
  options = {
-		-- numbers = function(opts)
-		--     return string.format('%s·%s', opts.raise(opts.id), opts.lower(opts.ordinal))
-		-- end,
-    -- number_style = "superscript",
-    -- mappings = false,
+	 	numbers = "none",
     close_command = "bdelete! %d",
     right_mouse_command = "bdelete! %d",
     left_mouse_command = "buffer %d",
     middle_mouse_command = nil,
+    indicator_icon = '▎',
 		buffer_close_icon = "",
     modified_icon = "",
     close_icon = "",
@@ -19,11 +21,12 @@ require("bufferline").setup{
     tab_size = 20,
 		view = "default",
 		offsets = {{filetype = "NvimTree", text = "File Explorer", text_align = "left", padding = 0}},
-    diagnostics = "nvim_lsp",
-		diagnostics_indicator = function(count, level, diagnostics_dict, context)
-			local icon = level:match("error") and " " or " "
-			return " " .. icon .. count
-		end,
+		diagnostics = false,
+		--     diagnostics = "nvim_lsp",
+		-- diagnostics_indicator = function(count, level, diagnostics_dict, context)
+		-- 	local icon = level:match("error") and " " or " "
+		-- 	return " " .. icon .. count
+		-- end,
     show_buffer_icons = true,
     show_buffer_close_icons = true,
     show_close_icon = true,
