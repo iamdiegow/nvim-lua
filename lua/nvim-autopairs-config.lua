c0089c6a23a67364bf08d6e1fe1343079c403be9
Diego Weinmann <diegoweinmann@gmail.com>
2021-12-15 00:08:18 -0300

    nvim-autopairs: add require check

--- lua/nvim-autopairs-config.lua
+++ lua/nvim-autopairs-config.lua
@@ -1,4 +1,10 @@
-require('nvim-autopairs').setup({
+local status_ok, nvim_autopairs = pcall(require, 'nvim-autopairs')
+if not status_ok then
+	return
+end
+
+nvim_autopairs.setup({
   disable_filetype = { "TelescopePrompt" , "vim" },
-	check_ts = true
+	check_ts = true,
+	ts_config = {}
 })
