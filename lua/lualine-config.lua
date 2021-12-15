99029726482ff2ae2be3ec34ef41b695ed4584ca
Diego Weinmann <diegoweinmann@gmail.com>
2021-12-15 00:39:09 -0300

    lualine: update config

--- lua/lualine-config.lua
+++ lua/lualine-config.lua
@@ -1,3 +1,8 @@
+local status_ok, lualine = pcall(require, 'lualine')
+if not status_ok then
+	return
+end
+
 local window_width_limit = 80
 
 local function diff_source()
@@ -11,94 +16,90 @@ local function diff_source()
   end
 end
 
+local hide_in_width = function()
+	return vim.fn.winwidth(0) > 80
+end
+
 local conditions = {
   buffer_not_empty = function()
     return vim.fn.empty(vim.fn.expand "%:t") ~= 1
   end,
   hide_in_width = function()
     return vim.fn.winwidth(0) > window_width_limit
-  end,
-  -- check_git_workspace = function()
-  --   local filepath = vim.fn.expand "%:p:h"
-  --   local gitdir = vim.fn.finddir(".git", filepath .. ";")
-  --   return gitdir and #gitdir > 0 and #gitdir < #filepath
-  -- end,
+  end
 }
 
-require("lualine").setup({
+local filetype = {
+ "filetype",
+	condition = conditions.hide_in_width
+}
+
+local diagnostics = {
+	{ "diagnostics",
+		sources = { "nvim_diagnostic" },
+		sections = {"error", "warn"},
+		symbols = { error = " ", warn = " " },
+		color = {},
+		condition = conditions.hide_in_width,
+		colored = false,
+		always_visible = true
+	},
+}
+
+local diff = {
+	"diff",
+	source = diff_source,
+	symbols = { added = "  ", modified = "柳", removed = " " },
+	color_added = "#a7c080",
+	color_modified = "#ffdf1b",
+	color_removed = "#ff6666",
+	color = {},
+	cond = hide_in_width,
+}
+
+local branch = {
+	"branch",
+	"b:gitsigns_head",
+	icons_enabled = true,
+	icon = "",
+	color = {gui = "bold"}
+}
+
+lualine.setup({
   options = {
     icons_enabled = true,
     theme = vim.g.color_theme,
-		-- component_separators = {'', ''},
-		-- section_separators = {'', ''},
 		section_separators = {' ', ' '},
 		component_separators = {' ', ' '},
-		-- section_separators = {'', ''},
-    --section_separators = { "", "" },
-		--component_separators = { " ", " " },
-    disabled_filetypes = {
-			"dapui_scopes",
-			"dapui_breakpoints",
-			"dapui_stacks",
-			"dapui_watches",
-			"dap-repl"
-		},
 		left_padding = 1,
 		right_padding = 1
   },
   sections = {
     lualine_a = {{"mode"}, "paste" },
-    lualine_b = {
-      { "diagnostics",
-				sources = { "nvim_diagnostic" },
-				symbols = { error = " ", warn = " ", info = " ", hint = " " },
-				color = {},
-				condition = conditions.hide_in_width
-			},
-			-- {
-			-- 	"filename",
-			-- 	function()
-			-- 		return "%="
-			-- 	end,
-			-- }
-    },
-		lualine_c = {},
-    lualine_x = {
-			{ "filetype",
-				condition = conditions.hide_in_width
-			}
+    lualine_b = diagnostics,
+		lualine_c = {
+			filetype
 		},
-    -- lualine_b = {
-    --   {
-    --     "progress",
-    --   },
-    -- },
+    lualine_x = {
+			"filename"
+		},
     lualine_y = {
-      { "branch", "b:gitsigns_head", icon = "", color = {gui = "bold"} },
-      {
-				"diff",
-				source = diff_source,
-				symbols = { added = "  ", modified = "柳", removed = " " },
-				color_added = "#a7c080",
-				color_modified = "#ffdf1b",
-				color_removed = "#ff6666",
-				color = {},
-				condition = nil,
-			},
+ 			branch,
+      diff
   	},
 			-- { "diff", color_added = "#a7c080", color_modified = "#ffdf1b", color_removed = "#ff6666" },
     lualine_z = {
       {
         "location",
         icon = "",
-				condition = conditions.hide_in_width
+				cond = hide_in_width
       },
     },
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = { "filename" },
+    lualine_c = {{ "filename", cond = hide_in_width }},
     lualine_x = { "location" },
     lualine_y = {},
     lualine_z = {},
