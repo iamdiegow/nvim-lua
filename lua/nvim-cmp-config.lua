79e6cdeefb0a68350fbf9784ad7f0cd4a8bf7b39
Diego Weinmann <diegoweinmann@gmail.com>
2021-12-18 23:20:39 -0300

    cmp: remove limit for lsp suggestions

--- lua/nvim-cmp-config.lua
+++ lua/nvim-cmp-config.lua
@@ -28,7 +28,7 @@ cmp.setup({
 		['<C-k>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
 	},
 	sources = {
-		{ name = 'nvim_lsp', max_item_count = 15 },
+		{ name = 'nvim_lsp' },
 		{ name = 'vsnip', max_item_count = 5 },
 		{ name = 'buffer', keyword_length = 5, max_item_count = 3 }
 	},
