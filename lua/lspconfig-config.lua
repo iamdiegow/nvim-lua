0755253bf7a0ce200b380e1b5a2bdc702153706a
Diego Weinmann <diegoweinmann@gmail.com>
2021-12-23 10:27:43 -0300

    lspconfig: update config

--- lua/lspconfig-config.lua
+++ lua/lspconfig-config.lua
@@ -73,26 +73,49 @@ require'lspconfig'.tsserver.setup {
   filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact" }
 }
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-		signs = {
-			severity = {
-				min = vim.diagnostic.severity.HINT
-			}
-		},
-    underline = {
-			severity = {
-				min = vim.diagnostic.severity.WARN
-			}
-		},
-		severity_sort = true,
-		update_in_insert = true,
-    virtual_text = {
-      spacing = 15,
-      prefix = ' ',
-    }
-  }
-)
+local config = {
+	-- disable virtual text
+	virtual_text = false,
+	-- show signs
+	signs = {
+		active = signs,
+	},
+	update_in_insert = true,
+	underline = true,
+	severity_sort = true,
+	float = {
+		focusable = false,
+		style = "minimal",
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+	},
+}
+
+vim.diagnostic.config(config)
+
+-- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+--   vim.lsp.diagnostic.on_publish_diagnostics, {
+-- 		signs = {
+-- 			severity = {
+-- 				min = vim.diagnostic.severity.HINT
+-- 			}
+-- 		},
+--     underline = {
+-- 			severity = {
+-- 				min = vim.diagnostic.severity.ERROR
+-- 			}
+-- 		},
+-- 		severity_sort = true,
+-- 		update_in_insert = false,
+--     -- virtual_text = {
+--     --   spacing = 15,
+--     --   prefix = ' ',
+--     -- }, 
+-- 		virtual_text = false
+--   }
+-- )
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
 	border = "rounded"
