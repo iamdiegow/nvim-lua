ccde8183358c2c2a0e740530710301910cf08155
Diego Weinmann <diegoweinmann@gmail.com>
2021-12-24 00:39:03 -0300

    dashboard: update config

--- lua/dashboard-config.lua
+++ lua/dashboard-config.lua
@@ -2,18 +2,10 @@ vim.g.dashboard_default_executive = "telescope"
 vim.g.dashboard_custom_header = {
     "",
 }
-
 vim.g.dashboard_custom_section = {
     a = {description = {"  Find File                 SPC f f"}, command = "Telescope find_files"},
     b = {description = {"  Recents                   SPC f o"}, command = "Telescope oldfiles"},
-    c = {description = {"  Find Word                 SPC f w"}, command = "Telescope live_grep"},
-    e = {description = {"  Bookmarks                 SPC b m"}, command = "Telescope marks"},
 }
-
-vim.g.dashboard_custom_footer = {
-    "NVIM"
-}
-
 vim.g.dashboard_custom_footer = {
 	"Nvim loaded in " .. vim.fn.printf("%.3f", vim.fn.reltimefloat(vim.fn.reltime(vim.g.start_time))) .. " seconds.",
 }
