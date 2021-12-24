ab8d0099dc718d4c40426d4fca8d99728e528abf
Diego Weinmann <diegoweinmann@gmail.com>
2021-12-24 01:08:56 -0300

    cmp: update config

--- lua/nvim-cmp-config.lua
+++ lua/nvim-cmp-config.lua
@@ -6,7 +6,45 @@ if not cmp_status_ok then
 	return
 end
 
-local lspkind = require'lspkind'
+-- local lspkind = require'lspkind'
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
+--   פּ ﯟ   some other good icons
+local kind_icons = {
+  Text = "",
+  Method = "m",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "",
+  Interface = "",
+  Module = "",
+  Property = "",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
+}
 
 cmp.setup({
 	snippet = {
@@ -16,34 +54,59 @@ cmp.setup({
 	},
 	mapping = {
 		-- normal mode
-		['<C-d>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.close(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }),
+		['<C-d>'] 		= cmp.mapping.scroll_docs(-2),
+		['<C-f>'] 		= cmp.mapping.scroll_docs(4),
+		['<C-e>'] 		= cmp.mapping {
+											i = cmp.mapping.abort(),
+											c = cmp.mapping.close()
+										},
+		['<CR>'] 			= cmp.mapping.confirm({ select = true }),
 		-- insert mode
-		['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-		['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
-		['<C-J>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-		['<C-k>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
+		["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif vim.fn["vsnip#available"](1) == 1 then
+        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-prev)", "")
+      end
+    end, { "i", "s" }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+		['<C-J>'] 		= cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+		['<C-k>'] 		= cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
 	},
 	sources = {
-		{ name = 'nvim_lsp', max_item_count = 15 },
-		{ name = 'vsnip', max_item_count = 3 },
+		{ name = 'nvim_lsp', keyword_length = 2, max_item_count = 15 },
+		{ name = 'vsnip', max_item_count = 5 },
 		{ name = 'buffer', keyword_length = 5, max_item_count = 3 },
 		{ name = 'path', max_item_count = 3 }
 	},
 	formatting = {
-		format = lspkind.cmp_format({
-			with_text = true,
-			maxwidth = 500,
-			menu = {
-				buffer = "[buf]",
-				nvim_lsp = "[LSP]",
-				vsnip = "[snip]"
-			}
-		})
+    fields = { "kind", "abbr", "menu" },
+    format = function(entry, vim_item)
+      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+      vim_item.menu = ({
+        nvim_lsp = "[LSP]",
+        vsnip = "[Snippet]",
+        buffer = "[Buffer]",
+        path = "[Path]",
+      })[entry.source.name]
+      return vim_item
+    end,
 	},
+  confirm_opts = {
+    behavior = cmp.ConfirmBehavior.Replace,
+    select = false,
+  },
   documentation = {
     border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
   },
