58b6a518acdf47e8f5b59c96d1237bbbf506a74f
Diego Weinmann <diegoweinmann@gmail.com>
2021-12-24 00:07:49 -0300

    plugins: refactor config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,19 +1,46 @@
-	local execute = vim.api.nvim_command
 local fn = vim.fn
 
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
+  PACKER_BOOTSTRAP = fn.system {
+		'git',
+		'clone',
+		'https://github.com/wbthomason/packer.nvim',
+		install_path,
+	}
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
 end
 
-return require('packer').startup(function(use)
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+local status_ok, packer = pcall(require, 'packer')
+if not status_ok then
+	return
+end
+
+packer.init {
+	display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+	}
+}
+
+return packer.startup(function(use)
   use 'wbthomason/packer.nvim'
 	-- Colorschemes
 	use 'Mofiqul/vscode.nvim'
 	use 'bluz71/vim-moonfly-colors'
 	use 'folke/tokyonight.nvim'
+  use "lunarvim/darkplus.nvim"
 	-- Lsp
   use 'neovim/nvim-lspconfig'
 	use 'kabouzeid/nvim-lspinstall'
@@ -71,5 +98,8 @@ return require('packer').startup(function(use)
 	}
 	use 'glepnir/dashboard-nvim'
 	use 'vimwiki/vimwiki'
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
 end)
 
