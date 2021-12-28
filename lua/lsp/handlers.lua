e5a45f2e6557e79cc2234bfb79348caeea45f0a5
Diego Weinmann <diegoweinmann@gmail.com>
2021-12-27 23:51:39 -0300

    lsp: disable Format binding

--- lua/lsp/handlers.lua
+++ lua/lsp/handlers.lua
@@ -82,7 +82,7 @@ local function lsp_keymaps(bufnr)
   map(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
   -- map(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
 
-  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+  -- vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
 end
 
 M.on_attach = function(client, bufnr)
