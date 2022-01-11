0604219df5e1ee89a8affec5c98c13741bf63a5b
Diego Weinmann <diegoweinmann@gmail.com>
2022-01-11 00:19:08 -0300

    autopairs: update config

--- lua/nvim-autopairs-config.lua
+++ lua/nvim-autopairs-config.lua
@@ -4,7 +4,18 @@ if not status_ok then
 end
 
 nvim_autopairs.setup({
-  disable_filetype = { "TelescopePrompt" , "vim" },
 	check_ts = true,
-	ts_config = {}
+  disable_filetype = { "TelescopePrompt" , "vim" },
+  ts_config = {
+    lua = { "string", "source" },
+    javascript = { "string", "template_string" },
+    java = false,
+  }
 })
+
+local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+  return
+end
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
