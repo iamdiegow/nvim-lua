5fc71defb510480e394d9795f73a01e3fbe44747
Diego Weinmann <diegoweinmann@gmail.com>
2022-02-20 00:26:37 -0300

    indent-blankline: update config

--- lua/indent-blankline-config.lua
+++ lua/indent-blankline-config.lua
@@ -4,38 +4,22 @@ if not status_ok then
 end
 
 vim.opt.termguicolors = true
--- vim.cmd [[highlight IndentBlanklineIndent1 guifg=gold blend=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent2 guifg=orchid blend=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent3 guifg=lightskyblue blend=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent4 guifg=lightgreen blend=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent5 guifg=pink blend=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent6 guifg=dodgerblue blend=nocombine]]
-
-vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
-vim.g.indent_blankline_filetype_exclude = {
-	"help",
-	"startify",
-	"dashboard",
-	"packer",
-	"neogitstatus",
-	"NvimTree",
-	"Trouble",
-}
 
 indent_blankline.setup {
-	space_char_blankline = " ",
-	buftype_exclude = {"terminal"},
-	filetype_exclude = {"help", "dashboard", "terminal", "NvimTree"},
+	char = "┆",
+	buftype_exclude = {"terminal", "nofile"},
+	filetype_exclude = {
+		"help",
+		"dashboard",
+		"packer",
+		"NvimTree",
+		"Trouble",
+	},
 	show_current_context = false,
+	show_current_context_start = false,
 	show_trailing_blankline_indent = false,
-	show_first_indent_level = false,
-	show_char_blankline = " ",
-	-- char_highlight_list = {
-	-- 		"IndentBlanklineIndent1",
-	-- 		"IndentBlanklineIndent2",
-	-- 		"IndentBlanklineIndent3",
-	-- 		"IndentBlanklineIndent4",
-	-- 		"IndentBlanklineIndent5",
-	-- 		"IndentBlanklineIndent6",
-	-- },
+	show_first_indent_level = true,
+	space_char_blankline = " ",
+	show_end_of_line = true,
+	use_treesitter = true
 }
