1a5c392d4c098407a919e0323abf3b29719d7d90
Diego Weinmann <diegoweinmann@gmail.com>
2022-03-29 00:17:54 -0300

    nvim-tree: update configg

--- lua/nvim-tree-config.lua
+++ lua/nvim-tree-config.lua
@@ -1,42 +1,4 @@
 local g = vim.g
---
--- default keymappings
-local nvim_tree_bindings = {
-    { key = {"<CR>", "l", "o"}, 						action = "edit" },
-	  { key = {"O"},                          action = "edit_no_picker" },
-		{ key = {"<2-RightMouse>", "<C-]>"},    action = "cd" },
-		{ key = "<C-v>",                        action = "vsplit" },
-		{ key = "<C-x>",                        action = "split" },
-		{ key = "<C-t>",                        action = "tabnew" },
-		{ key = "<",                            action = "prev_sibling" },
-		{ key = ">",                            action = "next_sibling" },
-		{ key = "P",                            action = "parent_node" },
-		{ key = { "<BS>", "h" },                action = "close_node" },
-	  --keeps the cursor in the tree
-		{ key = "<Tab>",                        action = "preview" },
-		{ key = "K",                            action = "first_sibling" },
-		{ key = "J",                            action = "last_sibling" },
-		{ key = "I",                            action = "toggle_ignored" },
-		{ key = "H",                            action = "toggle_dotfiles" },
-		{ key = "R",                            action = "refresh" },
-		{ key = "a",                            action = "create" },
-		{ key = "d",                            action = "remove" },
-		{ key = "D",                            action = "trash" },
-		{ key = "r",                            action = "rename" },
-		{ key = "<C-r>",                        action = "full_rename" },
-		{ key = "x",                            action = "cut" },
-		{ key = "c",                            action = "copy" },
-		{ key = "p",                            action = "paste" },
-		{ key = "y",                            action = "copy_name" },
-		{ key = "Y",                            action = "copy_path" },
-		{ key = "gy",                           action = "copy_absolute_path" },
-		{ key = "[c",                           action = "prev_git_item" },
-		{ key = "]c",                           action = "next_git_item" },
-		{ key = "-",                            action = "dir_up" },
-		{ key = "s",                            action = "system_open" },
-		{ key = "q",                            action = "close" },
-		{ key = "g?",                           action = "toggle_help" },
-}
 
 -- options that are not migrated yet
 
@@ -76,12 +38,13 @@ g.nvim_tree_icons = {
 }
 
 require('nvim-tree').setup {
+	auto_reload_on_write = true,
   disable_netrw       = true,
-  hijack_netrw        = true,
-  open_on_setup       = true,
+  hijack_netrw        = false,
+  open_on_setup       = true, -- open the tree if no file is provided
   quit_on_open        = true,
   ignore_ft_on_setup  = {},
-	auto_open = true,
+	auto_open 					= true,
   auto_close          = true,
   open_on_tab         = false,
   update_to_buf_dir   = {
@@ -119,12 +82,8 @@ require('nvim-tree').setup {
     width = 25,
 		height = 20,
     side = 'left',
-    auto_resize = false,
+    auto_resize = true,
 		number = false,
-    mappings = {
-      custom_only = false,
-      list = nvim_tree_bindings
-    },
   },
 	filters = {
 		dotfiles = false,
@@ -141,8 +100,28 @@ require('nvim-tree').setup {
 	trash = {
 		cmd = "trash",
 		require_confirm = true
+	},
+	actions = {
+		change_dir = {
+			enable = true,
+			global = false
+		},
+		open_file = {
+			quit_on_open = false,
+			resize_window = false,
+			window_picker = {
+				enable = true,
+				chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+				exclude = {
+					filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+					buftype = { "nofile", "terminal", "help" },
+				}
+			}
+		}
 	}
 }
 
+vim.o.splitright = true
+
 -- hide statusline when nvim tree is opened
-vim.cmd [[au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == "NvimTree" | set laststatus=0 | else | set laststatus=2 | endif]]
+vim.cmd [[au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == "NvimTree_1" | set laststatus=0 | else | set laststatus=2 | endif]]
