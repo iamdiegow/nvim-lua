f8381cfb9632ca056b6bfa6b715bd606d5fbe311
Diego Weinmann <diegoweinmann@gmail.com>
2022-03-29 00:17:33 -0300

    colorscheme: update colorscheme

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -13,14 +13,16 @@ end
 
 local tokyonightTheme = function()
 	-- 'night' | 'storm' | 'day'
-	vim.g.tokyonight_style = "night"
+	vim.g.tokyonight_style = "storm"
 	vim.g.tokyonight_terminal_colors = true
-	vim.g.tokyonight_italic_comments = true
+	vim.g.tokyonight_italic_comments = false
 	vim.g.tokyonight_italic_keywords = false
 	vim.g.tokyonight_italic_variables = false
 	vim.g.tokyonight_italic_variables = false
-	vim.g.tokyonight_dark_sidebar = true
+	vim.g.tokyonight_dark_sidebar = false
+	vim.g.tokyonight_dark_float = false
 	vim.g.tokyonight_lualine_bold = true
+	vim.g.tokyonight_sidebars = {"NvimTree", "vista_kind", "terminal", "packer"}
 	vim.cmd([[ colorscheme tokyonight ]])
 end
 
