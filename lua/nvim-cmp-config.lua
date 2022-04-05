91da29e76480acd21902137ad5fe164e37e55aa0
Diego Weinmann <diegoweinmann@gmail.com>
2022-04-05 00:48:56 -0300

    nvim-cmp: update config

--- lua/nvim-cmp-config.lua
+++ lua/nvim-cmp-config.lua
@@ -107,14 +107,14 @@ cmp.setup({
 		{ name = 'path', max_item_count = 3 }
 	},
 	formatting = {
-    fields = { "kind", "abbr", "menu" },
+    fields = { "menu", "kind", "abbr" },
     format = function(entry, vim_item)
       vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
       vim_item.menu = ({
-        nvim_lsp = "[LSP]",
-        luasnip = "[Luasnip]",
-        buffer = "[Buffer]",
-        path = "[Path]",
+        nvim_lsp = "[lsp]",
+        luasnip = "[luasnip]",
+        buffer = "[buffer]",
+        path = "[path]",
       })[entry.source.name]
       return vim_item
     end,
@@ -129,5 +129,30 @@ cmp.setup({
 	experimental = {
 		native_menu = false,
 		ghost_text = false
+	},
+	view = {
+		entries = {
+			name = "custom"
+		}
 	}
 })
+
+vim.cmd([[
+	" gray
+	highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#808080
+	" blue
+	highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6
+	highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#569CD6
+	" light blue
+	highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE
+	highlight! CmpItemKindInterface guibg=NONE guifg=#9CDCFE
+	highlight! CmpItemKindText guibg=NONE guifg=#9CDCFE
+	" pink
+	highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0
+	highlight! CmpItemKindMethod guibg=NONE guifg=#C586C0
+	" front
+	highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4
+	highlight! CmpItemKindProperty guibg=NONE guifg=#D4D4D4
+	highlight! CmpItemKindUnit guibg=NONE guifg=#D4D4D4
+]])
+
