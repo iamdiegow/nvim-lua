beba8ff2fdeb4c883e1dbcbf800d736111e48ed2
Diego Weinmann <diegoweinmann@gmail.com>
2022-04-20 23:57:32 -0300

    autocommands config update

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,4 +1,4 @@
--- Remove all trailing whitespace before save
+-- Remove all trailing whitespace before save -----
 vim.api.nvim_exec([[
   augroup TrimWhiteSpace
     au!
@@ -6,14 +6,24 @@ vim.api.nvim_exec([[
   augroup END
 ]], false)
 
--- Prevent new line to start with comment
+-- Prevent new line to start with comment -----
 vim.cmd([[
 	autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
 ]])
 
+-- get to last cursor position -----
 vim.cmd([[
 	augroup last-cursor-position
 			autocmd!
 			autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
+	augroup END
+]])
+
+-- Highlight yank -----
+vim.cmd([[
+	augroup AuYank
+		autocmd!
+		autocmd TextYankPost *
+			\ lua vim.highlight.on_yank{higroup="IncSearch", timeout=300, on_visual=true}
 	augroup END
 ]])
