00a90c93e1c95c07d2e1964e298c2eb7cccc0196
Diego Weinmann <diegoweinmann@gmail.com>
2022-07-12 19:28:03 -0300

    nvim-cmp: update config

--- lua/nvim-cmp-config.lua
+++ lua/nvim-cmp-config.lua
@@ -70,32 +70,36 @@ cmp.setup({
 		['<C-b>'] = cmp.mapping.scroll_docs(-2),
 		['<C-f>'] = cmp.mapping.scroll_docs(2),
 		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
+		['<C-c>'] = cmp.mapping.abort(),
 		['<CR>'] = cmp.mapping.confirm({ select = true }),
 		['<C-j>'] = cmp.mapping.select_next_item(),
 		['<C-k>'] = cmp.mapping.select_prev_item(),
 		['<Tab>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expandable() then
-				luasnip.expand()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			elseif check_backspace() then
-				fallback()
-			else
-				fallback()
-			end
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.jumpable(1) then
+        luasnip.jump(1)
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif luasnip.expandable() then
+        luasnip.expand()
+      elseif check_backspace() then
+        -- cmp.complete()
+        fallback()
+      else
+        fallback()
+      end
 		end),
 		['<S-Tab>'] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
 			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
+					luasnip.jump(-1)
 			else
 				fallback()
 			end
-		end),
+			end
+		),
 	}),
 	sources = cmp.config.sources({
 		{ name = 'nvim_lsp', keyword_length = 2, max_item_count = 10} ,
@@ -120,7 +124,10 @@ cmp.setup({
   confirm_opts = {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
-  }
+  },
+	experimental = {
+		ghost_text = true
+	}
 })
 
 vim.cmd([[
