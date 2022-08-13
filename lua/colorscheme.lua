7cd8cd50f1cb2912316042e93d6181ee227e7a29
Diego Weinmann <diegoweinmann@gmail.com>
2022-08-13 15:09:41 -0300

    colorscheme

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -67,9 +67,11 @@ local cobalt2Theme = function()
 end
 
 local catppuccinTheme = function()
-	vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
+	vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+	local colors = require('catppuccin.palettes').get_palette()
+	colors.nono = "NONE"
 	require('catppuccin').setup({
-		transparent_background = false,
+		transparent_background = true,
 		styles = {
 			comments = { "italic" },
 			conditionals = { "italic" },
@@ -98,14 +100,22 @@ local catppuccinTheme = function()
 			hop = true,
 			navic = true,
 			bufferline = false
+		},
+		custom_highlights = {
+			Comment = { fg = colors.overlay1 },
+			LineNr = { fg = colors.overlay1 },
+			CursorLine = { bg = colors.none },
+			CursorLineNr = { fg = colors.lavender },
+			DiagnosticVirtualTextError = { bg = colors.none },
+			DiagnosticVirtualTextWarn = { bg = colors.none },
+			DiagnosticVirtualTextInfo = { bg = colors.none },
+			DiagnosticVirtualTextHint = { bg = colors.none },
 		}
 	})
 	vim.cmd([[colorscheme catppuccin]])
 end
 
 local selectTheme = {
-	moonfly = moonflyTheme,
-	vscode = vscodeTheme,
 	tokyonight = tokyonightTheme,
 	darkplus = darkplusTheme,
 	dracula = draculaTheme,
