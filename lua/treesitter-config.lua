38a48963d284433d11506ecbe8b63ea598ef3b4c
Diego Weinmann <diegoweinmann@gmail.com>
2022-09-11 17:30:19 -0300

    treesitter: add incremental selection

--- lua/treesitter-config.lua
+++ lua/treesitter-config.lua
@@ -62,6 +62,15 @@ require("nvim-treesitter.configs").setup({
 			},
 		},
 	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "<CR>",
+			node_incremental = "<CR>",
+			scope_incremental = "<BS>",
+			node_decremental = "<BS>",
+		},
+	},
 })
 
 require("treesitter-context").setup({
