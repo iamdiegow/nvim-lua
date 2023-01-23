02c7d6e504dcea56bbcaa61395d4b2434e87e0cf
Diego Weinmann <diegoweinmann@gmail.com>
2023-01-22 21:38:22 -0300

    refactor lsp

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,4 +1,2 @@
 require('lsp.mason')
-require('lsp.null-ls')
 require('lsp.handlers').setup()
-require('lsp.lsp-saga')
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,140 @@
+return {
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	"jose-elias-alvarez/typescript.nvim",
+	"L3MON4D3/LuaSnip",
+	"rafamadriz/friendly-snippets",
+
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		event = "VeryLazy",
+		config = function()
+			local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+			if not null_ls_status_ok then
+				return
+			end
+
+			-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+			local formatting = null_ls.builtins.formatting
+			-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+			local diagnostics = null_ls.builtins.diagnostics
+			local code_actions = null_ls.builtins.code_actions
+
+			null_ls.setup({
+				debounce = 1000,
+				debug = false,
+				diagnostics_format = "[#{c}] #{m} (#{s})",
+				sources = {
+					diagnostics.eslint_d.with({
+						condition = function(utils)
+							return utils.root_has_file({ ".eslintrc", ".eslintrc.js", ".eslintrc.json" })
+						end,
+					}),
+					code_actions.eslint_d,
+					formatting.black.with({ extra_args = { "--fast" } }),
+					formatting.prettierd.with({
+						filetypes = {
+							"javascript",
+							"javascriptreact",
+							"typescript",
+							"typescriptreact",
+							"css",
+							"scss",
+							"html",
+							"json",
+							"yaml",
+							"graphql",
+						},
+						prefer_local = "node_modules/.bin",
+					}),
+					formatting.stylua,
+				},
+				update_in_insert = true,
+			})
+		end,
+	},
+	{
+		"glepnir/lspsaga.nvim",
+		event = "VeryLazy",
+		config = function()
+			local status_ok, saga = pcall(require, "lspsaga")
+			if not status_ok then
+				return
+			end
+
+			saga.setup({
+				preview = {
+					lines_above = 0,
+					lines_below = 10,
+				},
+				scroll_preview = {
+					scroll_down = "<C-j>",
+					scroll_up = "<C-k>",
+				},
+				request_timeout = 2000,
+				move_in_saga = { prev = "<C-k>", next = "<C-j>" },
+				diagnostic_header = { " ", " ", " ", "ﴞ " },
+				finder = {
+					edit = { "o", "<CR>" },
+					vsplit = "s",
+					split = "i",
+					tabe = "t",
+					quit = { "q", "<ESC>" },
+				},
+				code_action = {
+					num_shortcut = true,
+					keys = {
+						quit = "q",
+						exec = "<CR>",
+					},
+				},
+				lightbulb = {
+					enable = true,
+					virtual_text = true,
+					sign = false,
+					enable_in_insert = false,
+				},
+				diagnostic = {
+					show_code_action = true,
+					show_source = true,
+					jump_num_shorcut = true,
+					keys = {
+						exec_action = "o",
+						quit = "q",
+						go_action = "g",
+					},
+				},
+				rename = {
+					quit = "<C-c>",
+					exec = "<CR>",
+					mark = "x",
+					confirm = "<CR>",
+					in_select = true,
+					whole_project = false,
+				},
+				outline = {
+					win_position = "right",
+					show_detail = true,
+					auto_preview = false,
+					auto_refresh = true,
+					auto_close = true,
+					keys = {
+						jump = "o",
+						expand_collapse = "u",
+						quit = "q",
+					},
+				},
+				symbol_in_winbar = {
+					enable = true,
+					separator = "  ",
+					hide_keyword = false,
+					show_file = true,
+					folder_level = 2,
+					respect_root = false,
+					color_mode = false,
+				},
+			})
+		end,
+	},
+}
