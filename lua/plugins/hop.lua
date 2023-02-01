bdd0cd9c6a5488c0d448b55f8fd88545e62317ca
Diego Weinmann <diegoweinmann@gmail.com>
2023-01-31 23:50:14 -0300

    fix hop keymaps

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -26,6 +26,17 @@ return {
 			"t",
 			function()
 				require("hop").hint_char1({
+					direction = require("hop.hint").HintDirection.AFTER_CURSOR,
+					current_line_only = false,
+					hint_offset = -1,
+				})
+			end,
+			desc = "Hop t",
+		},
+		{
+			"T",
+			function()
+				require("hop").hint_char1({
 					direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
 					current_line_only = false,
 					hint_offset = -1,
