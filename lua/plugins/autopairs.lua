98c716fce4183a5ba568324e85af1db6ef4178f6
Diego Weinmann <diegoweinmann@gmail.com>
2023-02-04 13:08:16 -0300

    make autopairs a dependency of cmp

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,7 +1,6 @@
 return {
 	{
 		"windwp/nvim-autopairs",
-		event = "InsertEnter",
 		config = function()
 			local status_ok, nvim_autopairs = pcall(require, "nvim-autopairs")
 			if not status_ok then
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -172,6 +172,7 @@ return {
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
 			"saadparwaiz1/cmp_luasnip",
+			"windwp/nvim-autopairs",
 		},
 	},
 }
