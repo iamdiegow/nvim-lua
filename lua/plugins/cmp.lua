de606fa3f2af4fffb66fbd693d0709fcff602805
Diego Weinmann <diegoweinmann@gmail.com>
2023-02-08 22:20:33 -0300

    refactor: move autopairs as a dependency of cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -208,7 +208,38 @@ return {
 			"hrsh7th/cmp-nvim-lsp-document-symbol",
 			"hrsh7th/cmp-nvim-lsp-signature-help",
 			"saadparwaiz1/cmp_luasnip",
-			"windwp/nvim-autopairs",
+			{
+				"windwp/nvim-autopairs",
+				config = function()
+					local status_ok, nvim_autopairs = pcall(require, "nvim-autopairs")
+					if not status_ok then
+						return
+					end
+
+					nvim_autopairs.setup({
+						check_ts = true,
+						disable_filetype = { "TelescopePrompt", "vim" },
+						ts_config = {
+							lua = { "string", "source" },
+							javascript = { "string", "template_string" },
+							java = false,
+						},
+						map_char = {
+							all = "(",
+							tex = "{",
+						},
+						ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]], "%s+", ""),
+						enable_bracket_in_quote = false,
+					})
+
+					local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+					local cmp_status_ok, cmp = pcall(require, "cmp")
+					if not cmp_status_ok then
+						return
+					end
+					cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
+				end,
+			},
 		},
 	},
 }
