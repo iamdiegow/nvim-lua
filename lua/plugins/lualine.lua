dff5a4c4e40115d8f54b625ddb2e6480ef3ff40c
Diego Weinmann <diegoweinmann@gmail.com>
2023-02-14 21:04:06 -0300

    refactor: remove snippets

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -83,7 +83,7 @@ return {
 			lualine.setup({
 				options = {
 					icons_enabled = true,
-					theme = "tokyonight",
+					theme = "gruvbox",
 					component_separators = { left = "", right = "" },
 					section_separators = { left = "", right = "" },
 					left_padding = 0,
