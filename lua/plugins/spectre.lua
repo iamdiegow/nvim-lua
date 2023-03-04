9c53f8297c2cd85b53c449273631a03e527256bf
Diego Weinmann <diegoweinmann@gmail.com>
2023-03-04 16:58:32 -0300

    spectre: update keys

--- lua/plugins/spectre.lua
+++ lua/plugins/spectre.lua
@@ -2,11 +2,25 @@ return {
 	"windwp/nvim-spectre",
 	keys = {
 		{
-			"<leader>S",
+			"<leader>So",
 			function()
 				require("spectre").open()
 			end,
 			desc = "Replace in files (Spectre)",
+		},
+		{
+			"<leader>Sw",
+			function()
+				require("spectre").open_visual({ select_word = true })
+			end,
+			desc = "Open visual - select word (Spectre)",
+		},
+		{
+			"<leader>Sf",
+			function()
+				require("spectre").open_file_search()
+			end,
+			desc = "File Search (Spectre)",
 		},
 	},
 }
