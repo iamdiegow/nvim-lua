034a46265047bfe30274451f6ff186f50d747071
Diego Weinmann <diegoweinmann@gmail.com>
2023-03-05 17:39:24 -0300

    refactor colorschemes

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,41 @@
+return {
+	{
+		"folke/tokyonight.nvim",
+		enabled = false,
+		lazy = false,
+		priority = 1000,
+		opts = {
+			transparent = false,
+			style = "night",
+			styles = {
+				comments = { italic = true },
+				keywords = { italic = false, bold = false },
+				sidebars = "normal",
+				floats = "normal",
+			},
+		},
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		enabled = true,
+		lazy = false,
+		priority = 1000,
+		config = function()
+			local colors = require("gruvbox.palette").colors
+			require("gruvbox").setup({
+				contrast = "dark",
+				overrides = {
+					Pmenu = { bg = colors.dark0_hard  },
+					SignColumn = { bg = colors.dark0_hard},
+					GitSignsAdd = { bg = colors.dark0_hard, fg = colors.bright_green },
+					GitSignsChange = { bg = colors.dark0_hard, fg = colors.bright_aqua },
+					GitSignsDelete = { bg = colors.dark0_hard, fg = colors.bright_red },
+					DiagnosticSignWarn = { bg = colors.dark0_hard, fg = colors.bright_yellow },
+					DiagnosticSignInfo = { bg = colors.dark0_hard, fg = colors.bright_blue },
+					DiagnosticSignHint = { bg = colors.dark0_hard, fg = colors.bright_aqua },
+					DiagnosticSignError = { bg = colors.dark0_hard, fg = colors.bright_red },
+				},
+			})
+		end,
+	},
+}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,48 +1,5 @@
 return {
 	"folke/lazy.nvim",
-
-	-- Colorschemes
-	{
-		"folke/tokyonight.nvim",
-		enabled = false,
-		lazy = false,
-		priority = 1000,
-		opts = {
-			transparent = false,
-			style = "night",
-			styles = {
-				comments = { italic = true },
-				keywords = { italic = false, bold = false },
-				sidebars = "normal",
-				floats = "normal",
-			},
-		},
-	},
-
-	{
-		"ellisonleao/gruvbox.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			local colors = require("gruvbox.palette").colors
-			require("gruvbox").setup({
-				contrast = "dark",
-				overrides = {
-					Pmenu = { bg = colors.dark0_hard  },
-					SignColumn = { bg = colors.dark0_hard},
-					GitSignsAdd = { bg = colors.dark0_hard, fg = colors.bright_green },
-					GitSignsChange = { bg = colors.dark0_hard, fg = colors.bright_aqua },
-					GitSignsDelete = { bg = colors.dark0_hard, fg = colors.bright_red },
-					DiagnosticSignWarn = { bg = colors.dark0_hard, fg = colors.bright_yellow },
-					DiagnosticSignInfo = { bg = colors.dark0_hard, fg = colors.bright_blue },
-					DiagnosticSignHint = { bg = colors.dark0_hard, fg = colors.bright_aqua },
-					DiagnosticSignError = { bg = colors.dark0_hard, fg = colors.bright_red },
-
-				},
-			})
-		end,
-	},
-
 	-- Workflow
 	{
 		"famiu/bufdelete.nvim",
@@ -77,7 +34,6 @@ return {
 		end,
 	},
 	{
-
 		"folke/zen-mode.nvim",
 		cmd = "ZenMode",
 		keys = {
