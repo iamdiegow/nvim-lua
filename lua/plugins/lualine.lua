f38cb4c7fe2e0251f8350607315516cfc14a083f
Diego Weinmann <diegoweinmann@gmail.com>
2023-03-11 01:24:33 -0300

    lualine: clean config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,62 +1,42 @@
+local branch = {
+	"branch",
+	"b:gitsigns_head",
+	icons_enabled = true,
+	icon = "",
+	color = { gui = "bold" },
+}
+
+local filename = {
+	"filename",
+	path = 3,
+	file_status = true,
+	shorting_target = 30,
+	component_separators = { left = "", right = "" },
+}
+
 return {
 	{
 		"nvim-lualine/lualine.nvim",
-		event = "VeryLazy",
-		config = function()
-			local branch = {
-				"branch",
-				"b:gitsigns_head",
+		lazy = false,
+		opts = {
+			options = {
 				icons_enabled = true,
-				icon = "",
-				color = { gui = "bold" },
-			}
-
-			local filename = {
-				"filename",
-				path = 3,
-				file_status = true,
-				shorting_target = 30,
-				component_separators = { left = "", right = "" },
-			}
-
-			require("lualine").setup({
-				options = {
-					icons_enabled = true,
-					theme = "tokyonight",
-					component_separators = { left = "", right = "" },
-					section_separators = { left = "", right = "" },
-					left_padding = 0,
-					right_padding = 0,
-					disabled_filetypes = {
-						"Outline",
-						"NvimTree",
-					},
-					always_divide_middle = true,
-					globalstatus = true,
+				theme = "tokyonight",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {
+					"Outline",
+					"NvimTree",
 				},
-				sections = {
-					lualine_a = { { "mode" } },
-					lualine_b = { branch },
-					lualine_c = { filename },
-					lualine_x = {},
-					lualine_y = {},
-					lualine_z = { function()
-						return " " .. os.date("%R")
-					end },
-				},
-				inactive_sections = {
-					lualine_a = {},
-					lualine_b = {},
-					lualine_c = {},
-					lualine_x = {},
-					lualine_y = {},
-					lualine_z = {},
-				},
-				tabline = {},
-				extensions = {},
-			})
-
-			vim.cmd([[ au BufEnter * :set laststatus=3 ]])
-		end,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { branch },
+				lualine_c = { filename },
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {},
+			},
+		},
 	},
 }
