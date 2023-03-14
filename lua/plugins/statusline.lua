4ed257a54c985efa41bb089aa638478b1c751539
Diego Weinmann <diegoweinmann@gmail.com>
2023-03-13 23:55:14 -0300

    statusline: add staline

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -14,8 +14,11 @@ local filename = {
 	component_separators = { left = "", right = "" },
 }
 
+local colors = require("tokyonight.colors")
+
 return {
 	{
+		enabled = false,
 		"nvim-lualine/lualine.nvim",
 		lazy = false,
 		opts = {
@@ -36,6 +39,37 @@ return {
 				lualine_x = {},
 				lualine_y = { filename },
 				lualine_z = {},
+			},
+		},
+	},
+	{
+		"tamton-aquib/staline.nvim",
+		event = "BufReadPre",
+		opts = {
+			sections = {
+				left = { " ", "mode", " ", "lsp" },
+				mid = { "file_name" },
+				right = { "branch", " " },
+			},
+			mode_colors = {
+				i = colors.green,
+				n = colors.blue,
+				c = colors.yellow,
+				v = colors.orange,
+				V = colors.orange,
+			},
+			mode_icons = {
+				i = "INSERT",
+				n = "NORMAL",
+				c = "COMMAND",
+				v = "VISUAL",
+				V = "VISUAL",
+			},
+			defaults = {
+				true_colors = true,
+				full_path = true,
+				branch_symbol = " ",
+				mod_symbol = " ",
 			},
 		},
 	},
