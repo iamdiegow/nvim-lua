4f3fbee7ef229b8bfb5bb821e7025aa970e97269
Diego Weinmann <diegoweinmann@gmail.com>
2023-03-21 20:59:23 -0300

    bufferline: update style

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -11,17 +11,17 @@ return {
 			middle_mouse_command = nil,
 			--"⎟ ⎜ ⎢ ⎜ ▏ ▊ ▋ ▉ ▎ ▍ ▋ ▍ ▎ "
 			indicator = {
-				icon = "▊",
+				icon = "▍",
 			},
 			buffer_close_icon = "",
 			modified_icon = "",
 			close_icon = "",
 			left_trunc_marker = "",
 			right_trunc_marker = "",
-			max_name_length = 20,
+			max_name_length = 15,
 			max_prefix_length = 15,
 			truncate_names = true,
-			tab_size = 20,
+			tab_size = 15,
 			offsets = {
 				{
 					filetype = "NvimTree",
@@ -35,7 +35,7 @@ return {
 			},
 			diagnostics = false,
 			color_icons = true,
-			show_buffer_icons = true,
+			show_buffer_icons = false,
 			show_buffer_close_icons = false,
 			show_close_icon = false,
 			show_tab_indicators = true,
