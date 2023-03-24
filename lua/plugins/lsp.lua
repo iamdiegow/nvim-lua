39f6410f0860093c58df398c874511f22f836baa
Diego Weinmann <diegoweinmann@gmail.com>
2023-03-24 14:52:47 -0300

    null-ls: event

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,7 +12,7 @@ return {
 	"jose-elias-alvarez/typescript.nvim",
 	{
 		"jose-elias-alvarez/null-ls.nvim",
-		event = { "BufReadPre", "BufNewFile" },
+		event = "LspAttach",
 		config = function()
 			local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 			if not null_ls_status_ok then
