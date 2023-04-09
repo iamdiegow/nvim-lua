ced1e360e9b0e31f0adb4fab06aaad159661bfc8
Diego Weinmann <diegoweinmann@gmail.com>
2023-04-09 15:39:08 -0300

    alpha: replace icon

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -20,7 +20,7 @@ return {
 				" " .. " Old files",
 				":lua require('telescope.builtin').oldfiles({ cwd = vim.fn.getcwd() })<CR>"
 			),
-			dashboard.button("r", " " .. " Restore session for CWD", ":lua require('persistence').load()<CR>"),
+			dashboard.button("r", "󰸧 " .. " Restore session for CWD", ":lua require('persistence').load()<CR>"),
 			dashboard.button("l", "鈴" .. " Lazy", ":Lazy<CR>"),
 			dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 		}
