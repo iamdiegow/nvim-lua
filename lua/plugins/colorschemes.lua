6f7c56bba53762613cfe9a1569de55a439ffbbf1
Diego Weinmann <diegoweinmann@gmail.com>
2023-04-22 22:40:04 -0300

    colorscheme: update style

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -6,7 +6,7 @@ return {
 		priority = 1000,
 		opts = {
 			transparent = true,
-			style = "moon",
+			style = "storm",
 			styles = {
 				comments = { italic = true },
 				keywords = { italic = false, bold = true },
