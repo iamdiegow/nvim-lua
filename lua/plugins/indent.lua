42374a7d585725b8f3873cd2dd39af2735172d8b
Diego Weinmann <diegoweinmann@gmail.com>
2023-05-09 20:48:36 -0300

    indent: switch plugin

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		"lukas-reineke/indent-blankline.nvim",
-		enabled = false,
+		enabled = true,
 		event = "BufReadPost",
 		keys = {
 			{
@@ -43,12 +43,18 @@ return {
 	},
 	{
 		"echasnovski/mini.indentscope",
+		enabled = false,
 		event = "VeryLazy",
 		version = "*",
 		config = function()
 			require("mini.indentscope").setup({
 				draw = {
 					delay = 1000,
+				},
+				--"⎟ ⎜ ⎢ ⎜ ▏ ▊ ▋ ▉ ▎ ▍ ▋ ▍ ▎╎"
+				symbol = "▍",
+				options = {
+					border = "top",
 				},
 			})
 		end,
