ff23f562964337e5feb74ce4bb61f40704661cff
Diego Weinmann <diegoweinmann@gmail.com>
2023-08-17 19:08:32 -0300

    neogit: update config

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -98,68 +98,200 @@ return {
 	{
 		"TimUntersberger/neogit",
 		keys = {
-			{ "<leader>gg", "<cmd>Neogit<CR>", desc = "Neogit" },
+			{ "<leader>gg", "<cmd>lua require('neogit').open()<CR>", desc = "Open Neogit" },
 		},
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"sindrets/diffview.nvim",
 		},
-		opts = {
-			disable_signs = false,
-			disable_hint = false,
-			disable_context_highlighting = false,
-			disable_commit_confirmation = false,
-			auto_refresh = true,
-			disable_builtin_notifications = false,
-			use_magit_keybindings = false,
-			kind = "tab",
-			commit_popup = {
-				kind = "split",
-			},
-			popup = {
-				kind = "split",
-			},
-			signs = {
-				section = { ">", "v" }, -- { CLOSED, OPENED }
-				item = { ">", "v" },
-				hunk = { "", "" },
-			},
-			integrations = {
-				diffview = true,
-			},
-			-- Setting any section to `false` will make the section not render at all
-			sections = {
-				untracked = {
-					folded = false,
+		config = function()
+			require("neogit").setup({
+				-- Hides the hints at the top of the status buffer
+				disable_hint = false,
+				-- Disables changing the buffer highlights based on where the cursor is.
+				disable_context_highlighting = false,
+				-- Disables signs for sections/items/hunks
+				disable_signs = false,
+				-- Do not ask to confirm the commit - just do it when the buffer is closed.
+				disable_commit_confirmation = false,
+				-- Uses `vim.notify` instead of the built-in notification system.
+				disable_builtin_notifications = false,
+				-- Changes what mode the Commit Editor starts in. `true` will leave nvim in normal mode, `false` will change nvim to insert mode, and `"auto"` will change nvim to insert mode IF the commit message is empty, otherwise leaving it in normal mode.
+				disable_insert_on_commit = true,
+				-- Allows a different telescope sorter. Defaults to 'fuzzy_with_index_bias'. The example below will use the native fzf
+				-- sorter instead. By default, this function returns `nil`.
+				telescope_sorter = function()
+					return require("telescope").extensions.fzf.native_fzf_sorter()
+				end,
+				-- Persist the values of switches/options within and across sessions
+				remember_settings = true,
+				-- Scope persisted settings on a per-project basis
+				use_per_project_settings = true,
+				-- Table of settings to never persist. Uses format "Filetype--cli-value"
+				ignored_settings = {
+					"NeogitPushPopup--force-with-lease",
+					"NeogitPushPopup--force",
+					"NeogitPullPopup--rebase",
+					"NeogitCommitPopup--allow-empty",
+					"NeogitRevertPopup--no-edit",
 				},
-				unstaged = {
-					folded = false,
-				},
-				staged = {
-					folded = false,
-				},
-				stashes = {
-					folded = true,
-				},
-				unpulled = {
-					folded = true,
-				},
-				unmerged = {
-					folded = false,
-				},
-				recent = {
-					folded = true,
-				},
-			},
-			-- override/add mappings
-			mappings = {
-				-- modify status buffer mappings
+				-- Neogit refreshes its internal state after specific events, which can be expensive depending on the repository size.
+				-- Disabling `auto_refresh` will make it so you have to manually refresh the status after you open it.
+				auto_refresh = true,
+				-- Value used for `--sort` option for `git branch` command
+				-- By default, branches will be sorted by commit date descending
+				-- Flag description: https://git-scm.com/docs/git-branch#Documentation/git-branch.txt---sortltkeygt
+				-- Sorting keys: https://git-scm.com/docs/git-for-each-ref#_options
+				sort_branches = "-committerdate",
+				-- Change the default way of opening neogit
+				kind = "tab",
+				-- The time after which an output console is shown for slow running commands
+				console_timeout = 2000,
+				-- Automatically show console if a command takes more than console_timeout milliseconds
+				auto_show_console = true,
 				status = {
-					-- Adds a mapping with "B" as key that does the "BranchPopup" command
-					["B"] = "BranchPopup",
+					recent_commit_count = 10,
 				},
-			},
-		},
+				commit_editor = {
+					kind = "split",
+				},
+				commit_select_view = {
+					kind = "tab",
+				},
+				commit_view = {
+					kind = "vsplit",
+				},
+				log_view = {
+					kind = "tab",
+				},
+				rebase_editor = {
+					kind = "split",
+				},
+				reflog_view = {
+					kind = "tab",
+				},
+				merge_editor = {
+					kind = "split",
+				},
+				preview_buffer = {
+					kind = "split",
+				},
+				popup = {
+					kind = "split",
+				},
+				signs = {
+					-- { CLOSED, OPENED }
+					hunk = { "", "" },
+					item = { ">", "v" },
+					section = { ">", "v" },
+				},
+				integrations = {
+					diffview = true,
+				},
+				sections = {
+					-- Reverting/Cherry Picking
+					sequencer = {
+						folded = false,
+						hidden = false,
+					},
+					untracked = {
+						folded = false,
+						hidden = false,
+					},
+					unstaged = {
+						folded = false,
+						hidden = false,
+					},
+					staged = {
+						folded = false,
+						hidden = false,
+					},
+					stashes = {
+						folded = true,
+						hidden = false,
+					},
+					unpulled_upstream = {
+						folded = true,
+						hidden = false,
+					},
+					unmerged_upstream = {
+						folded = false,
+						hidden = false,
+					},
+					unpulled_pushRemote = {
+						folded = true,
+						hidden = false,
+					},
+					unmerged_pushRemote = {
+						folded = false,
+						hidden = false,
+					},
+					recent = {
+						folded = true,
+						hidden = false,
+					},
+					rebase = {
+						folded = true,
+						hidden = false,
+					},
+				},
+				mappings = {
+					finder = {
+						["<cr>"] = "Select",
+						["<c-c>"] = "Close",
+						["<esc>"] = "Close",
+						["<c-n>"] = "Next",
+						["<c-p>"] = "Previous",
+						["<down>"] = "Next",
+						["<up>"] = "Previous",
+						["<tab>"] = "MultiselectToggleNext",
+						["<s-tab>"] = "MultiselectTogglePrevious",
+						["<c-j>"] = "NOP",
+					},
+					-- Setting any of these to `false` will disable the mapping.
+					status = {
+						["q"] = "Close",
+						["I"] = "InitRepo",
+						["1"] = "Depth1",
+						["2"] = "Depth2",
+						["3"] = "Depth3",
+						["4"] = "Depth4",
+						["<tab>"] = "Toggle",
+						["x"] = "Discard",
+						["s"] = "Stage",
+						["S"] = "StageUnstaged",
+						["<c-s>"] = "StageAll",
+						["u"] = "Unstage",
+						["U"] = "UnstageStaged",
+						["d"] = "DiffAtFile",
+						["$"] = "CommandHistory",
+						["#"] = "Console",
+						["<c-r>"] = "RefreshBuffer",
+						["<enter>"] = "GoToFile",
+						["<c-v>"] = "VSplitOpen",
+						["<c-x>"] = "SplitOpen",
+						["<c-t>"] = "TabOpen",
+						["?"] = "HelpPopup",
+						["D"] = "DiffPopup",
+						["p"] = "PullPopup",
+						["r"] = "RebasePopup",
+						["m"] = "MergePopup",
+						["P"] = "PushPopup",
+						["c"] = "CommitPopup",
+						["l"] = "LogPopup",
+						["v"] = "RevertPopup",
+						["Z"] = "StashPopup",
+						["A"] = "CherryPickPopup",
+						["b"] = "BranchPopup",
+						["f"] = "FetchPopup",
+						["X"] = "ResetPopup",
+						["M"] = "RemotePopup",
+						["{"] = "GoToPreviousHunkHeader",
+						["}"] = "GoToNextHunkHeader",
+					},
+				},
+			})
+		end,
 	},
 	{
 		"lewis6991/gitsigns.nvim",
