b08333307b8d34dd86acb6ee539d76e559809e54
Diego Weinmann <diegoweinmann@gmail.com>
2023-10-02 22:25:57 -0300

    indent-blank: update config

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -3,43 +3,14 @@ return {
 		"lukas-reineke/indent-blankline.nvim",
 		enabled = true,
 		event = "BufReadPost",
+		main = "ibl",
 		keys = {
 			{
 				"<leader>ig",
-				"<cmd>IndentBlanklineToggle<CR>",
+				"<cmd>IBLToggle<CR>",
 				{ desc = "Toggle indent line" },
 			},
 		},
-		config = function()
-			local status_ok, indent_blankline = pcall(require, "indent_blankline")
-			if not status_ok then
-				return
-			end
-
-			vim.opt.termguicolors = true
-
-			vim.cmd([[
-	highlight IndentBlanklineContextStart guisp=#FFFFFF gui=bold
-	highlight IndentBlanklineContextChar guifg=#FFFFFF gui=bold
-]])
-
-			indent_blankline.setup({
-				enabled = false,
-				char = "│",
-				buftype_exclude = { "terminal", "nofile" },
-				filetype_exclude = {
-					"help",
-					"dashboard",
-					"packer",
-					"NvimTree",
-					"Trouble",
-				},
-				show_first_indent_level = true,
-				space_char_blankline = " ",
-				show_end_of_line = true,
-				use_treesitter = true,
-			})
-		end,
 	},
 	{
 		"echasnovski/mini.indentscope",
