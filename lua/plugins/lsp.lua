ebfdf5fd6b1b05b791d4dff24826cc6c03e5bc7f
Diego Weinmann <diegoweinmann@gmail.com>
2024-02-24 23:36:40 -0300

    error-lens: remove plugin

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -62,29 +62,4 @@ return {
 			})
 		end,
 	},
-	{
-		"chikko80/error-lens.nvim",
-		enabled = false,
-		event = "BufRead",
-		dependencies = {
-			"nvim-telescope/telescope.nvim",
-		},
-		opts = {
-			enabled = true,
-			fallback_bg_color = nil,
-			step = 7,
-			total = 30,
-			prefix = 5,
-			colors = {
-				error_fg = "#FF6363",
-				error_bg = "#4B252C",
-				warn_fg = "#FA973A",
-				warn_bg = "#403733",
-				info_fg = "#5B38E8",
-				info_bg = "#281478",
-				hint_fg = "#25E64B",
-				hint_bg = "#147828",
-			},
-		},
-	},
 }
