634903cf1b9adf44581d207f5f8b0b805d7637e7
Diego Weinmann <diegoweinmann@gmail.com>
2024-03-10 22:58:06 -0300

    settings: add autoread

--- lua/settings.lua
+++ lua/settings.lua
@@ -21,6 +21,7 @@ vim.opt.confirm = true
 vim.opt.splitbelow = true -- Put new windows below current
 vim.opt.splitright = true -- Put new windows right of current
 vim.opt.splitkeep = "screen"
+vim.opt.autoread = true
 
 wo.number = true
 wo.numberwidth = 2
