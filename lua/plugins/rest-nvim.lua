15b78541f46d3806e7bab78155bb4a16dffd8eab
Diego Weinmann <diegoweinmann@gmail.com>
2024-03-18 22:34:34 -0300

    rest: update to v2

--- lua/plugins/rest-nvim.lua
+++ lua/plugins/rest-nvim.lua
@@ -1,34 +1,56 @@
 return {
-	"rest-nvim/rest.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
+	{
+		"vhyrro/luarocks.nvim",
+		enabled = false,
+		config = function()
+			require("luarocks").setup({
+				rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+			})
+		end,
 	},
-	ft = { "http" },
-	keys = {
-		{
-			"<leader>rr",
-			"<Plug>RestNvim<CR>",
-			desc = "Run request under cursor (Rest Nvim)",
+	{
+		"rest-nvim/rest.nvim",
+		dependencies = {
+			"vhyrro/luarocks.nvim",
 		},
-		{
-			"<leader>rl",
-			"<Plug>RestNvimLast<CR>",
-			desc = "Re-Run last request (Rest Nvim)",
-		},
-	},
-	config = function()
-		require("rest-nvim").setup({
-			skip_ssl_verification = false,
-			result_split_horizontal = false,
-			stay_in_current_window_after_split = false,
-			encode_url = true,
-			result = {
-				-- URL, HTTP info, headers at the top of the result
-				show_url = true,
-				show_curl_command = false,
-				show_http_info = true,
-				show_headers = true,
+		ft = { "http" },
+		keys = {
+			{
+				"<leader>rr",
+				"<cmd>Rest run<CR>",
+				desc = "Run request under cursor (Rest Nvim)",
 			},
-		})
-	end,
+			{
+				"<leader>rl",
+				"<cmd>Rest run last<CR>",
+				desc = "Re-Run last request (Rest Nvim)",
+			},
+		},
+		config = function()
+			require("rest-nvim").setup({
+				skip_ssl_verification = false,
+				encode_url = true,
+				result = {
+					split = {
+						horizontal = false,
+						in_place = false,
+						stay_in_current_window_after_split = true,
+					},
+					behavior = {
+						decode_url = true,
+						show_info = {
+							url = true,
+							headers = true,
+							http_info = true,
+							curl_command = false,
+						},
+					},
+				},
+				highlight = {
+					enable = false,
+					timeout = 100,
+				},
+			})
+		end,
+	},
 }
