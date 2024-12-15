1bf47ee282b579297a3c5a1eb0e11bd1c9f2095b
Diego Weinmann <diego.weinmann@hotmail.com>
2024-12-15 11:56:11 -0300

    telescope: update keys entry

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,46 +1,44 @@
-local keys = {
-	{ "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Find Buffers (Telescope)" },
-	{ "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find Files (Telescope)" },
-	{
-		"<leader>f/",
-		function()
-			require("telescope.builtin").current_buffer_fuzzy_find({
-				prompt_title = " Fuzzy Find Buffer",
-			})
-		end,
-		desc = "Fuzy find buffer (Telescope)",
-	},
-	{ "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Live Grep (Telescope)" },
-	{ "<leader>fc", "<cmd>Telescope grep_string<CR>", desc = "Search String (Telescope)" },
-	{ "<leader>fh", "<cmd>Telescope help_tags<CR>", desc = "Find Help Tags (Telescope)" },
-	{
-		"<leader>fo",
-		function()
-			require("telescope.builtin").oldfiles({ cwd = vim.fn.getcwd() })
-		end,
-		desc = "Find Old files in CWD (Telescope)",
-	},
-	{ "<leader>fO", "<cmd>Telescope oldfiles<CR>", desc = "Find All Old Files (Telescope)" },
-	{ "<leader>fp", "<cmd>Telescope resume<CR>", desc = "Resume Last Search (Telescope)" },
-	{ "<leader>fk", "<cmd>Telescope keymaps<CR>", desc = "Find Keymaps (Telescope)" },
-	{ "<leader>fl", "<cmd>Telescope git_status<CR>", desc = "Git Status (Telescope)" },
-
-	{ "<leader>fr", "<cmd>Telescope lsp_references<CR>", desc = "Find References (Telescope)" },
-	{ "<leader>lr", "<cmd>Telescope lsp_references<CR>", desc = "Find References (Telescope)" },
-
-	{ "<leader>fd", "<cmd>Telescope diagnostics bufrn=0<CR>", desc = "Find Document Diagnostics (Telescope)" },
-	{ "<leader>ld", "<cmd>Telescope diagnostics bufrn=0<CR>", desc = "Find Document Diagnostics (Telescope)" },
-
-	{ "<leader>fs", "<cmd>Telescope lsp_document_symbols<CR>", desc = "Find Symbols (Telescope)" },
-	{ "<leader>ls", "<cmd>Telescope lsp_document_symbols<CR>", desc = "Find Symbols (Telescope)" },
-
-	{ "<leader>fi", "<cmd>Telescope lsp_implementations<CR>", desc = "Find Implementations (Telescope)" },
-	{ "<leader>li", "<cmd>Telescope lsp_implementations<CR>", desc = "Find Implementations (Telescope)" },
-}
-
 return {
 	"nvim-telescope/telescope.nvim",
-	keys = keys,
+	keys = {
+		{ "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Find Buffers (Telescope)" },
+		{ "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find Files (Telescope)" },
+		{
+			"<leader>f/",
+			function()
+				require("telescope.builtin").current_buffer_fuzzy_find({
+					prompt_title = " Fuzzy Find Buffer",
+				})
+			end,
+			desc = "Fuzy find buffer (Telescope)",
+		},
+		{ "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Live Grep (Telescope)" },
+		{ "<leader>fc", "<cmd>Telescope grep_string<CR>", desc = "Search String (Telescope)" },
+		{ "<leader>fh", "<cmd>Telescope help_tags<CR>", desc = "Find Help Tags (Telescope)" },
+		{
+			"<leader>fo",
+			function()
+				require("telescope.builtin").oldfiles({ cwd = vim.fn.getcwd() })
+			end,
+			desc = "Find Old files in CWD (Telescope)",
+		},
+		{ "<leader>fO", "<cmd>Telescope oldfiles<CR>", desc = "Find All Old Files (Telescope)" },
+		{ "<leader>fp", "<cmd>Telescope resume<CR>", desc = "Resume Last Search (Telescope)" },
+		{ "<leader>fk", "<cmd>Telescope keymaps<CR>", desc = "Find Keymaps (Telescope)" },
+		{ "<leader>fl", "<cmd>Telescope git_status<CR>", desc = "Git Status (Telescope)" },
+
+		{ "<leader>fr", "<cmd>Telescope lsp_references<CR>", desc = "Find References (Telescope)" },
+		{ "<leader>lr", "<cmd>Telescope lsp_references<CR>", desc = "Find References (Telescope)" },
+
+		{ "<leader>fd", "<cmd>Telescope diagnostics bufrn=0<CR>", desc = "Find Document Diagnostics (Telescope)" },
+		{ "<leader>ld", "<cmd>Telescope diagnostics bufrn=0<CR>", desc = "Find Document Diagnostics (Telescope)" },
+
+		{ "<leader>fs", "<cmd>Telescope lsp_document_symbols<CR>", desc = "Find Symbols (Telescope)" },
+		{ "<leader>ls", "<cmd>Telescope lsp_document_symbols<CR>", desc = "Find Symbols (Telescope)" },
+
+		{ "<leader>fi", "<cmd>Telescope lsp_implementations<CR>", desc = "Find Implementations (Telescope)" },
+		{ "<leader>li", "<cmd>Telescope lsp_implementations<CR>", desc = "Find Implementations (Telescope)" },
+	},
 	cmd = "Telescope",
 	dependencies = {
 		{ "nvim-lua/popup.nvim" },
@@ -67,6 +65,7 @@ return {
 				selection_strategy = "reset",
 				sorting_strategy = "descending",
 				scroll_strategy = "cycle",
+				path_display = { "filename_first" },
 				prompt_prefix = " ",
 				selection_caret = " ",
 				initial_mode = "insert",
