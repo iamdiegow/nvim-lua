d7b66c0630c9d9d489beaef1300920b90473bcfe
Diego Weinmann <diego.weinmann@hotmail.com>
2025-07-26 14:28:34 -0300

    incline: add plugin

---
+++ lua/plugins/incline.lua
@@ -0,0 +1,36 @@
+return {
+	{
+		"b0o/incline.nvim",
+		config = function()
+			require("incline").setup({
+				window = {
+					margin = {
+						horizontal = 0,
+						vertical = 0,
+					},
+					placement = {
+						horizontal = "right",
+						vertical = "top",
+					},
+					overlap = {
+						winbar = false,
+					},
+				},
+				render = function(props)
+					local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":.")
+					local modified = vim.bo[props.buf].modified
+
+					return {
+						" ",
+						filename,
+						modified and { " [+]", guifg = "#888888", gui = "bold" } or "",
+						" ",
+						guibg = "#111111",
+						guifg = "#eeeeee",
+					}
+				end,
+			})
+		end,
+		event = "VeryLazy",
+	},
+}
