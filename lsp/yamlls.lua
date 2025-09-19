f7135d43bd3cef02cf041c01dae623cc6b48b2c1
Diego Weinmann <diego.weinmann@hotmail.com>
2025-09-19 00:55:41 -0300

    yamlls: update docker-compose schema

--- lsp/yamlls.lua
+++ lsp/yamlls.lua
@@ -3,10 +3,10 @@ local opts = {
 		yaml = {
 			schemas = {
 				["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-				["https://raw.githubusercontent.com/docker/cli/master/cli/compose/schema/data/config_schema_v3.9.json"] = "docker-compose.yml"
-			}
-		}
-	}
+				["https://raw.githubusercontent.com/docker/cli/master/cli/compose/schema/data/config_schema_v3.13.json"] = "docker-compose.yml",
+			},
+		},
+	},
 }
 
 return opts
